ffb5622d9ac393fdfd871d775f7f784ff6c88d45
Alisson Medeiros <alisson.smpw@gmail.com>
2026-03-13 15:46:15 -0300

    verificação google search console

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,10 @@ export const metadata = {
   title: "ARCOMEX | Perfuração e Desmonte de Rocha",
   description:
     "Empresa especializada em perfuração e desmonte de rocha para obras de infraestrutura, terraplenagem e operações controladas.",
+  verification: {
+    google: "DCHdDvSOiQ0HxQ9DY1-w7fIBXySOgmoXGsL3oLrwAtk",
+  },
 };
-
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
