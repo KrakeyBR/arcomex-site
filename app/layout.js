5c5c94f49ee1b3a71ff7b25658a31646bff40b77
Alisson Medeiros <alisson.smpw@gmail.com>
2026-04-30 09:29:38 -0300

    localizar google

--- app/layout.js
+++ app/layout.js
@@ -20,11 +20,60 @@ export default function RootLayout({ children }) {
             __html: JSON.stringify({
               "@context": "https://schema.org",
               "@type": "LocalBusiness",
-              name: "ARCOMEX",
+              name: "ARCOMEX - Soluções em Desmonte",
               url: "https://arcomex.com.br",
-              description:
-                "Empresa especializada em perfuração e desmonte controlado de rocha.",
+              logo: "https://arcomex.com.br/images/logo-arcomex.png",
+              image: "https://arcomex.com.br/images/logo-arcomex.png",
+              telephone: "+55 62 99645-2450",
+              email: "arcomexltda@hotmail.com",
+              address: {
+                "@type": "PostalAddress",
+                streetAddress: "Estrada Pirenópolis-Pireneus Km 04, Fazenda Meia Lua",
+                addressLocality: "Pirenópolis",
+                addressRegion: "GO",
+                addressCountry: "BR",
+              },
               areaServed: "Brasil",
+              priceRange: "$$$",
+              description:
+                "Empresa especializada em perfuração e desmonte de rocha para obras de infraestrutura, terraplenagem, loteamentos e áreas urbanas controladas.",
+              makesOffer: [
+                {
+                  "@type": "Offer",
+                  itemOffered: {
+                    "@type": "Service",
+                    name: "Perfuração de rocha",
+                  },
+                },
+                {
+                  "@type": "Offer",
+                  itemOffered: {
+                    "@type": "Service",
+                    name: "Desmonte de rocha com explosivos",
+                  },
+                },
+                {
+                  "@type": "Offer",
+                  itemOffered: {
+                    "@type": "Service",
+                    name: "Desmonte controlado em área urbana",
+                  },
+                },
+                {
+                  "@type": "Offer",
+                  itemOffered: {
+                    "@type": "Service",
+                    name: "Terraplenagem",
+                  },
+                },
+                {
+                  "@type": "Offer",
+                  itemOffered: {
+                    "@type": "Service",
+                    name: "Estabilização de taludes",
+                  },
+                },
+              ],
             }),
           }}
         />
